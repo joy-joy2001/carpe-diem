5d88181211e04761328f51f03816a7b0eb6160e0
joy-joy2001 <jefiannayi@gmail.com>
2022-07-05 15:47:59 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import render_template
+from flask import url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/kanban-board")
+def kanban():
+    return render_template("kanban-board.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("signup.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
