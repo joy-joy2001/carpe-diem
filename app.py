fdc57a6b3c74e6a1b5cafd0cda6ab374f4750168
Joy <66254898+joy-joy2001@users.noreply.github.com>
2022-08-27 12:21:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
     date_created = db.Column(db.String(50), nullable=False)
     table_id = db.Column(db.String(80), nullable=False)
 
